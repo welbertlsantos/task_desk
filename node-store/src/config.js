135366b49d7dc725f95f8663ab7932b36fe359b6
welbert santos <welbert.l.santos@gmail.com>
2019-04-25 23:29:58 -0300

    Alteraçõe finais do projeto

--- node-store/src/config.js
+++
@@ -1,7 +0,0 @@
-global.CHAVE_PRIVADA =  'SNFNl7KDMW';
-
-module.exports = {
-    conectionString: 'mongodb://localhost:27017/taskdesk',
-    sendKey : 'WLS',
-    containerConnectionString : 'WLS'
-}
